2948c5f9560d37babfa680685e7b723b997f66f6
LeninGF <enteatenea@gmail.com>
2024-04-01 17:01:10 -0500

    update queries

--- queries.sql
+++ queries.sql
@@ -16,3 +16,35 @@ CONCAT(ROUND(COUNT(re.NDD) / SUM(COUNT(re.NDD)) OVER() * 100, 2), '%') AS 'Perce
 from reportes.relatosPoliciaSiaf_20230830 re 
 group by re.Tipo_Delito_PJ_comision
 ORDER BY COUNT(re.NDD) DESC;
+
+## robosML
+alter table DaaS.robosML add delitos_seguimiento_unified_origin TEXT,
+add delitos_seguimiento_unified_siaf_origin TEXT,
+add delitos_validados_unified_siaf_origin TEXT;
+
+UPDATE DaaS.robosML t
+INNER JOIN DaaS.robosML_predicted_tmp  s ON t.NDD = s.NDD
+SET t.delitos_seguimiento_unified_origin = s.delitos_seguimiento_unified_origin,
+t.delitos_seguimiento_unified_siaf_origin = s.delitos_seguimiento_unified_siaf_origin,
+t.delitos_validados_unified_siaf_origin = s.delitos_validados_unified_siaf_origin;
+
+
+UPDATE DaaS.robosML
+SET delitos_seguimiento_unified_siaf = CASE
+    WHEN delitos_seguimiento_unified_siaf = 'ROBO DE ACCESORIOS DE VEHICULOS' THEN 'ROBO DE BIENES, ACCESORIOS Y AUTOPARTES DE VEHICULOS'
+    WHEN delitos_seguimiento_unified_siaf = 'ROBO DE BIENES A ENTIDAD PUBLICA' THEN 'OTROS ROBOS'
+    WHEN delitos_seguimiento_unified_siaf =  'ROBO DE BIENES PERSONALES AL INTERIOR DEL VEHICULO' THEN 'ROBO DE BIENES, ACCESORIOS Y AUTOPARTES DE VEHICULOS'
+    WHEN delitos_seguimiento_unified_siaf =  'ROBO DE VEHICULOS' THEN 'ROBO DE CARROS'
+    WHEN delitos_seguimiento_unified_siaf =  'ROBO EN VIAS O CARRETERAS' THEN 'OTROS ROBOS'
+    ELSE delitos_seguimiento_unified_siaf  -- Keep other values unchanged
+END;
+
+UPDATE DaaS.robosML
+SET delitos_validados_unified_siaf = CASE
+    WHEN delitos_validados_unified_siaf = 'ROBO DE ACCESORIOS DE VEHICULOS' THEN 'ROBO DE BIENES, ACCESORIOS Y AUTOPARTES DE VEHICULOS'
+    WHEN delitos_validados_unified_siaf = 'ROBO DE BIENES A ENTIDAD PUBLICA' THEN 'ROBO EN INSTITUCIONES PUBLICAS'
+    WHEN delitos_validados_unified_siaf =  'ROBO DE BIENES PERSONALES AL INTERIOR DEL VEHICULO' THEN 'ROBO DE BIENES, ACCESORIOS Y AUTOPARTES DE VEHICULOS'
+    WHEN delitos_validados_unified_siaf =  'ROBO DE VEHICULOS' THEN 'ROBO DE CARROS'
+    WHEN delitos_validados_unified_siaf =  'ROBO EN VIAS O CARRETERAS' THEN 'OTROS ROBOS'
+    ELSE delitos_validados_unified_siaf  -- Keep other values unchanged
+END;
